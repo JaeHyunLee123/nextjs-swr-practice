57881ddc23de637993472549bdc21a934e2a9dab
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-16 20:51:06 +0900

    feat: add css to UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,16 +14,20 @@ export default () => {
   console.log(data);
 
   return (
-    <div>
-      <h1>Woof.tv</h1>
+    <div className="p-10 bg-slate-800 h-screen">
+      <h1 className="text-white text-6xl font-bold mb-10">Woof.tv</h1>
       {isValidating ? (
-        <h2>Loading...</h2>
+        <h2 className="text-white text-4xl font-bold">Loading...</h2>
       ) : (
-        <div>
-          <video src={data?.url}></video>
-          <div>
-            <button>New Dog!</button>
-            <button>Like</button>
+        <div className="flex flex-col justify-center items-center bg-slate-700 w-full rounded py-5 space-y-5">
+          <video className="h-[400px] max-w-full" src={data?.url} />
+          <div className="flex justify-center items-center w-full space-x-5">
+            <button className="cursor-pointer bg-white rounded py-2 w-5/12">
+              New Dog!
+            </button>
+            <button className="cursor-pointer text-white bg-blue-400 rounded py-2 w-5/12">
+              Like
+            </button>
           </div>
         </div>
       )}
