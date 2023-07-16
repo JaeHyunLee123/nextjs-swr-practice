113cd87604b6f591fec9683b17e984664cc153f2
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-16 20:20:29 +0900

    Initial commit with water.css setted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,23 @@
 import { SWRConfig } from "swr";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: any) {
   return (
     <SWRConfig
       value={{
-        fetcher: (url: string) => fetch(url).then((response) => response.json())
+        fetcher: (url: string) =>
+          fetch(url).then((response) => response.json()),
       }}
     >
-      <Component {...pageProps} />
+      <>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.min.css"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </>
     </SWRConfig>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
 import React from "react";
 
-export default () => <div></div>;
+export default () => (
+  <div>
+    <h1>Hello world!</h1>
+  </div>
+);
