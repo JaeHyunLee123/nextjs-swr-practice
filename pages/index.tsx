7f10d232457ee7d777ec56a37f0c9fabe9978ce2
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-16 20:36:28 +0900

    feat: Set basic UI and fetch data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import useSWR from "swr";
 
-export default () => (
-  <div>
-    <h1>Hello world!</h1>
-  </div>
-);
+interface IDogVideo {
+  url: string;
+  isLiked: boolean;
+}
+
+export default () => {
+  const { data, isValidating } = useSWR<IDogVideo>(
+    "https://dogs-api.nomadcoders.workers.dev"
+  );
+
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Woof.tv</h1>
+      {isValidating ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div>
+          <video src={data?.url}></video>
+          <div>
+            <button>New Dog!</button>
+            <button>Like</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
